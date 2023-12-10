397d33982d9af75ae031b4c2abf844335e306740
JamesKlouda <gusklouja@student.gu.se>
2023-12-10 20:06:39 +0100

    #3 (ongoing) Update email controller

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 // The program initializes a mqtt client and connects attempts to connect it to a broker.
 // The program initializes a mongo client and attempts to connect it, upon successful connection the db is pinged to prove connection.
-// The program uses nodemailer to send an email.
+// The program uses nodemailer to send an emails.
 const {client} = require('./src/mqtt/mqttUtils')
 const {dbClient, connectToDB} = require('./src/database/databaseHandler')
-require('./src/emailClient/nodemailer')
 
 console.log(client.options)
 
--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,1 +1,15 @@
-// EmailController will provide methods to build emails
+// EmailController provides methods to send emails
+const {sendEmail} = require('../email/nodemailer')
+const {newTimeslotsEmail} = require('../email/templates/newTimeslots.js')
+
+async function sendNewTimeslotsEmail() {
+    try {
+        await sendEmail(newTimeslotsEmail)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+module.exports = {
+    sendNewTimeslotsEmail
+};
