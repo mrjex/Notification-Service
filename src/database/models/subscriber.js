37b45fe72b68e2b9c4f8fb10ca5d702143fb1d98
JamesKlouda <gusklouja@student.gu.se>
2023-12-11 21:11:27 +0100

    #4 (ongoing) Add data validation model

--- src/database/models/subscriber.js
+++ src/database/models/subscriber.js
@@ -3,6 +3,25 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const subscriberSchema = new Schema({});
+const subscriberSchema = new Schema({
+    patient_ID: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: 'Email address is required',
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    clinic: {
+        type: String,
+        tags: [],
+        required: true
+    }
+});
 
-module.exports = mongoose.model('subscriber', subscriberSchema)
+const sub = mongoose.model('subscriber', subscriberSchema);
+
+module.exports = sub;
