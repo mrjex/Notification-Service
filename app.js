eec8c25a33ebd9d98f6e6ad0024ae9bd7c13e82b
JamesKlouda <gusklouja@student.gu.se>
2023-12-11 21:16:15 +0100

    #4 (completed) Update app

--- app.js
+++ app.js
@@ -2,14 +2,4 @@
 // The program initializes a mongo client and attempts to connect it, upon successful connection the db is pinged to prove connection.
 // The program uses nodemailer to send an emails.
 const {client} = require('./src/mqtt/mqttUtils')
-const {dbClient, connectToDB} = require('./src/database/databaseHandler')
-
-console.log(client.options)
-
-connectToDB()
-    .then(r => {
-        console.log('Successful')
-    })
-    .catch(error => {
-        console.log(error)
-    })
+const {resClient} = require('./src/mqtt/mqttResUtils')
