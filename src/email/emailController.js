1a5be0e6087ad35cffcc9c4e1ea52093e0b0ddc1
JamesKlouda <gusklouja@student.gu.se>
2023-12-12 04:07:46 +0100

    #3 (ongoing) Implement send emails to subscribers

--- src/email/emailController.js
+++ src/email/emailController.js
@@ -1,15 +1,53 @@
 // EmailController provides methods to send emails
+require('dotenv').config()
+const {dbClient} = require('../database/databaseClient')
+
 const {sendEmail} = require('./nodemailer')
 const {newTimeslotsEmail} = require('./templates/newTimeslots.js')
 
-async function sendNewTimeslotsEmail() {
+const dbName = process.env.DB_NAME
+
+// Returns an array of patients subscribed to a clinic
+async function getRecieverList(clinic){
+    let receiverList = []
     try {
+        const filter = { clinic: { $eq: clinic } }
+        
+        await dbClient.connect()
+        console.log('Opened db connection')
+        const db = dbClient.db(dbName)
+        const collection = db.collection("Subscribers")
+        const subscribers = collection.find(filter) // returns cursor
+        console.log('Looking for subscribers')
+        
+        for await (const doc of subscribers) {
+            receiverList.push(doc.email)
+          }
+          console.log(receiverList)
+          return receiverList
+    } catch(err) {
+        console.error(err)
+    } finally {
+        await dbClient.close();
+        console.log('Closed mongo connection')
+    }
+}
+
+async function sendNewTimeslotsEmail(topic) {
+    try {
+        const topics = topic.split('/')
+        const clinic = topics.pop()
+        console.log('CLINIC: ', clinic)
+
+        receiverList = await getRecieverList(clinic)
+        newTimeslotsEmail.to = receiverList
         await sendEmail(newTimeslotsEmail)
+        console.log(newTimeslotsEmail)
     } catch (err) {
         console.error(err)
     }
 }
 
 module.exports = {
-    sendNewTimeslotsEmail
+    sendNewTimeslotsEmail, getRecieverList
 };
