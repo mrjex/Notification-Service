4f777eff6de073797df6a545715414ea85088d15
JamesKlouda <gusklouja@student.gu.se>
2024-01-03 03:32:34 +0100

    #5 (ongoing) Update user requests
    This commit updates the user request to get users from the user service.

--- src/apiRequests/userRequests.js
+++ src/apiRequests/userRequests.js
@@ -1,30 +1,24 @@
 const axios = require('axios');
 const apiUrl = 'http://localhost:3000/';
 
-async function getPatient(patient_ID){ // TODO: Sync with api and user service to fetch actual patient.
-// axios.get(apiUrl + 'patients/:' + patient_ID)
-//   .then(response => {
-//     console.log('API Response:', response.data)
-//     return response.data
-//   })
-//   .catch(error => {
-//     console.error('Error fetching data from API:', error.message)
-//     return 'Could not retrive patient'
-//   })
-    return {name: 'Patient 1', email: 'gusklouja@student.gu.se'} 
+async function getPatient(patient_ID){
+  try {
+    const response = await axios.get(apiUrl + 'patients/' + patient_ID)
+    console.log('API Response:', response.data);
+    return response.data.patient;
+  } catch (error) {
+    throw new Error('UNABLE TO SEND EMAIL', 'CAUSE:', error.message)
+  }
 }
 
-async function getDentist(dentist_ID){ // TODO: Sync with api and user service to fetch actual dentist.
-  // axios.get(apiUrl + 'dentists/' + dentist_ID)
-  //   .then(response => {
-  //     console.log('API Response:', response.data)
-  //     return response.data
-  //   })
-  //   .catch(error => {
-  //     console.error('Error fetching data from API:', error.message)
-  //     return 'Could not retrive dentist'
-  //   })
-    return {name: 'Dentist 1', email: 'gusklouja@student.gu.se'} 
+async function getDentist(dentist_ID){
+  try {
+    const response = await axios.get(apiUrl + 'dentists/' + dentist_ID)
+    console.log('jhsadkjahsdkja', response.data)
+    return response.data.dentist
+    } catch (error) {
+      throw new Error('UNABLE TO SEND EMAIL', 'CAUSE:', error.message)
+    }
   }
 
 module.exports = { getPatient, getDentist }
