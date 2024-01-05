b895b8f8877ccd4650a731a605f92c45a57e1937
JamesKlouda <gusklouja@student.gu.se>
2024-01-05 17:40:08 +0100

    #9 (completed) Run cron jobs in app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // The program initializes a mqtt client and connects attempts to connect it to a broker.
 // The program initializes a mongo client and attempts to connect it, upon successful connection the db is pinged to prove connection.
 // The program uses nodemailer to send an emails.
+// The program runs cron jobs for resending failed emails and clearing map unsent emails every 24 h.
 const {client} = require('./src/mqtt/mqttReqUtils')
 const {resClient} = require('./src/mqtt/mqttResUtils')
+const {resilientEmails, clearUndeliveredEmailsMap} = require('./src/email/resilientEmailDelivery/cronJobs')
